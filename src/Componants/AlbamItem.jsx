c46494ab1af1ae432d01d5fe6e33f3a3e03138bd
satham002 <dev.hussain18@gmail.com>
2024-10-11 09:46:06 +0530

    Hosting on render platform

--- src/Componants/AlbamItem.jsx
+++ src/Componants/AlbamItem.jsx
@@ -5,7 +5,7 @@ const AlbamItem = ({image, name, desc, id}) => {
   const Navigator = useNavigate();
   return (
     <div onClick={()=>Navigator(`/albam/${id}`)} className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
-        <img src={image} className='rounded' alt="" />
+        <img src={image} className='rounded w-40' alt="" />
         <p className='font-bold mt-2 mb-1'>{name}</p>
         <p className='text-slate-200 text-sm'>{desc}</p>
     </div>
--- src/Componants/SongItem.jsx
+++ src/Componants/SongItem.jsx
@@ -5,7 +5,7 @@ const SongItem = ({name, image, id, desc}) => {
   const {playWidthId} = useContext(PlayerContext);
   return (
     <div onClick={()=>playWidthId(id)} className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
-      <img src={image} className='rounded' alt="" />
+      <img src={image} className='rounded w-40 h-40' alt="" />
       <p className='font-bold mt-2 mb-1'>{name}</p>
       <p className='text-slate-200 text-sm'>{desc}</p>
     </div>
